37a197b2534f0496beee899a83af864b716c9de0
IvanSolomakhin <solomakhinv@gmail.com>
2020-04-02 22:38:20 +0300

    Basic encoding completed

---
+++ lib/types.ts
@@ -0,0 +1,38 @@
+/**
+ * https://en.wikipedia.org/wiki/Magnet_URI_scheme
+ */
+
+export enum HASH {
+	TIGER_TREE_HASH = 'tree', // TODO
+	SECURE_HASH_ALGORITHM_1 = 'sha1', // TODO
+	BIT_PRINT = 'bitprint', // TODO
+	E_DONKEY_2000 = 'ed2k', // TODO
+	ADVANCED_INTELLIGENT_CORRUPTION_HANDLER = 'aich', // TODO
+	KAZAA_HASH = 'kzhash', // TODO
+	BIT_TORRENT_INFO_HASH = 'btih',
+	MESSAGE_DIGEST_5 = 'md5', // TODO
+}
+
+export enum MAGNET_PARAMETER {
+	DISPLAY_NAME = 'dn',
+	LENGTH = 'xl',
+	INFO_HASH = 'xt',
+	WEB_SEED = 'ws',
+	ACCEPTABLE_SOURCE = 'as',
+	SOURCE = 'xs',
+	KEYWORD = 'kt',
+	MANIFEST = 'mt',
+	TRACKER = 'tr',
+}
+
+export interface MagnetURI {
+	displayNames?: string[]; // a filename to display to the user, for convenience
+	length?: number; // size in bytes
+	infoHashes?: string[]; // URN containing file hash
+	webSeeds?: string[]; // the payload data served over HTTP(S)
+	acceptableSources?: string[]; // web link to the file online
+	sources?: string[]; // P2P link identified by a content-hash
+	keywords?: string[]; // a more general search, specifying keywords, rather than a particular file
+	manifest?: string; // link to the metafile that contains a list of magneto (MAGMA – MAGnet MAnifest)
+	trackers?: string[]; // tracker URL for BitTorrent downloads
+}
--- test/encode.test.ts
+++ test/encode.test.ts
@@ -1,9 +1,93 @@
+import * as assert from 'assert';
 import { encode } from '../lib';
 
 describe('Encoding tests', () => {
 
-	test('draft', () => {
-		encode({});
+	describe('Display name tests', () => {
+
+		test('should encode display name', () => {
+			const result = encode({ displayNames: [ 'test-name_for_magnet-link.tar.gz' ] });
+			assert.deepStrictEqual(result, 'magnet:?dn=test-name_for_magnet-link.tar.gz');
+		});
+
+	});
+
+	describe('Length tests', () => {
+
+		test('should encode length', () => {
+			const result = encode({ length: 100500 });
+			assert.deepStrictEqual(result, 'magnet:?xl=100500');
+		});
+
+	});
+
+	describe('Info hash tests' , () => {
+
+		test('should encode BitTorrent info hash', () => {
+			const result = encode({infoHashes: [ 'urn:btih:c12fe1c06bba254a9dc9f519b335aa7c1367a88a' ] });
+			assert.deepStrictEqual(result, 'magnet:?xt=urn:btih:c12fe1c06bba254a9dc9f519b335aa7c1367a88a');
+		});
+
+	});
+
+	describe('Web seed tests', () => {
+
+		test('should encode web seed', () => {
+			const result = encode({ webSeeds: [ 'http://download.wikimedia.org/mediawiki/1.15/mediawiki-1.15.1.tar.gz' ] });
+			assert.deepStrictEqual(result, 'magnet:?ws=http%3A%2F%2Fdownload.wikimedia.org%2Fmediawiki%2F1.15%2Fmediawiki-1.15.1.tar.gz');
+		});
+
+	});
+
+	describe('Acceptable source tests', () => {
+
+		test('should encode acceptable source', () => {
+			const result = encode({ acceptableSources: [ 'http://download.wikimedia.org/mediawiki/1.15/mediawiki-1.15.1.tar.gz' ] });
+			assert.deepStrictEqual(result, 'magnet:?as=http%3A%2F%2Fdownload.wikimedia.org%2Fmediawiki%2F1.15%2Fmediawiki-1.15.1.tar.gz');
+		});
+
+	});
+
+	describe('Source tests', () => {
+
+		test('should encode source', () => {
+			const result = encode({ sources: [
+				'http://cache.example.org/XRX2PEFXOOEJFRVUCX6HMZMKS5TWG4K5',
+				// 'dchub://example.org', // TODO: should it be encodeURIComponent() ?
+			] });
+			assert.deepStrictEqual(result, 'magnet:?xs=http%3A%2F%2Fcache.example.org%2FXRX2PEFXOOEJFRVUCX6HMZMKS5TWG4K5');
+		});
+
+	});
+
+	describe('Keyword tests', () => {
+
+		test('should encode keywords', () => {
+			const result = encode({ keywords: [ 'martin', 'luther', 'king', 'mp3' ] });
+			assert.deepStrictEqual(result, 'magnet:?kt=martin+luther+king+mp3');
+		});
+
+	});
+
+	describe('Manifest tests', () => {
+
+		test('should encode manifest', () => {
+			const result = encode({ manifest: 'http://weblog.foo/all-my-favorites.rss' });
+			assert.deepStrictEqual(result, 'magnet:?mt=http%3A%2F%2Fweblog.foo%2Fall-my-favorites.rss');
+		});
+
+	});
+
+	describe('Tracker tests', () => {
+
+		test('should encode trackers', () => {
+			const result = encode({ trackers: [
+				'http://tracker.example.org/announce.php?uk=1111111111&',
+				'wss://tracker.webtorrent.io',
+			] });
+			assert.deepStrictEqual(result, 'magnet:?tr=http%3A%2F%2Ftracker.example.org%2Fannounce.php%3Fuk%3D1111111111%26&tr=wss%3A%2F%2Ftracker.webtorrent.io');
+		});
+
 	});
 
 });
